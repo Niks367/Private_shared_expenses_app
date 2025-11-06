f7d04f2ffbff4122dbcf56167d868ed081f6df08
niks3 <nk@audite.ai>
2025-11-06 17:26:43 +0100

    Revert "Login"
    This reverts commit 7dbedb36b740760389cf306bfc0581e60f9c2a06.

--- app/src/main/java/com/example/myapplication/entities/Profile.kt
+++ app/src/main/java/com/example/myapplication/entities/Profile.kt
@@ -5,9 +5,9 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "profiles")
 data class Profile(
-        @PrimaryKey(autoGenerate = true) val id: String = 0,
+        @PrimaryKey(autoGenerate = true) val id: Long = 0,
         val name: String,
-        val email: String?,
+        val email: String,
         val phone: String?,
         val passwordHash: String   // store a hash, never plain text!
 )
